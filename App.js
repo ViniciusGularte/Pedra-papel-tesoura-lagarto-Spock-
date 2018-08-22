3aee1ec1889cc81332dfcac6e51e27e61d80a0e3
vinicius gularte martin <viniciusgulartemartin@gmail.com>
2018-08-22 13:05:58 -0300

    Iniciando logica do jogo

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,Button, Text, View } from 'react-native';
+
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {escolhaUsuario: ''}
+
+  }
+  jokenpo(escolhaUsuario){
+   this.setState({escolhaUsuario : escolhaUsuario});
+  }
   render() {
     return (
       <View style={styles.container}>
          <Text>Escolha do computador</Text>
-         <Text>Escolha do Usuario</Text>
+         <Text>Escolha do Usuario {this.state.escolhaUsuario}</Text>
          <Text>Resultado</Text>
+         <Button onPress={() => {this.jokenpo('pedra')}} title='pedra'/>
+         <Button onPress={() => {this.jokenpo('papel')}} title='papel'/>
+         <Button onPress={() => {this.jokenpo('tesoura')}} title='tesoura'/>
+         <Button onPress={() => {this.jokenpo('spock')}} title='spock'/>
+         <Button onPress={() => {this.jokenpo('lagarto')}} title='lagarto'/>
 
       </View>
     );
